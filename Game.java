fbf50e929fa9e6799bf136d6481ce8be9bf8ac26
MayraM5 <mayra.mercado@tuta.io>
2023-01-20 19:42:00 -0600

    added further study

--- Game.java
+++ Game.java
@@ -1,53 +1,84 @@
 import java.util.Scanner;
 import java.util.Random;
-
+import java.util.InputMismatchException;
+import java.util.Random;
+import java.util.InputMismatchException;
 
 /** A number guessing game. */
 public class Game {
   public static void main(String[] args) {
     System.out.println("Welcome to the Guessing Game!");
 
-
     Scanner input = new Scanner(System.in);
-
 
     // Get user's name
     System.out.println("Enter your name:");
     String name = input.nextLine();
-    // Test that everything works
     System.out.println(name +", I'm thinking of a number between 0 and 100");
-
 
     //Create an instance of the Random object
     Random rand = new Random();
 
-
     //Call method nextInt and pass in upper range (exclusive)
-    int number = rand.nextInt(100); // 101 to be inclusive //bound is automatically created by VSCode
+    int number = rand.nextInt(11); // 101 to be inclusive //bound is automatically created by VSCode
     System.out.println("Try to guess the number!");
 
+    int guessCounter = 0;
+    Boolean stillPlay = true;
+    int guess = -1;
 
-    int guessCounter = 1;
-   
-    System.out.println("What is your guess?");
-    int guess = input.nextInt();
-   
-    //int num = Integer.parseInt(str);
+    while (stillPlay) {
+      int playAgain = 2;
+      while (guess != number ) {
+        System.out.println("What is your guess?");
+        Boolean validGuess = false;
+          try {
+            guess = input.nextInt();
+            if (guess >= 0 && guess <= 10) {
+              validGuess = true;
+            } else {
+              System.out.println("Please enter a number between 1-100");
+            }
+          } catch(InputMismatchException e) {
+            String badInput = input.next();
+            System.out.println(badInput + " is not a valid number, try again");
+          }
 
-
-    while (guess != number) {
-      guessCounter++;
-      if (guess < number ) {
-        System.out.println("That's too low, please try again:");      
-        guess = input.nextInt();
-      } else {
-        System.out.println("That's too high, please try again:");      
-        guess = input.nextInt();
+        if (validGuess) {
+          guessCounter++;
+          if (guess < number ) {
+            System.out.println("That's too low, please try again:");
+          } else {
+            System.out.println("That's too high, please try again:");
+          }
+        }
       }
+
+        System.out.println("The number was " + number);
+        System.out.println("It took you " + guessCounter + " times to guess this number");
+        System.out.println("Enter 1 to play again, or 0 to quit");
+
+        while (playAgain != 1 && playAgain != 0) {
+          try {
+            playAgain = input.nextInt();
+
+            if (playAgain == 0) {
+              stillPlay = false;
+            } else if (playAgain == 1) {
+              number = rand.nextInt(10);
+              guess = -1;
+              guessCounter = 0;
+            } else {
+              System.out.println("Please enter 1 to play again, or 0 to quit");
+              playAgain = input.nextInt();
+            }
+
+          } catch (InputMismatchException e) {
+            String invalidInput = input.next();
+            System.out.println(invalidInput + " is invalid. Please enter a number(0 or 1)");
+            }
+        }
     }
-
-
-    System.out.println("The number was " + number);
-    System.out.println("It took you " + guessCounter + " times to guess this number");
+    System.out.println("Thanks for playing! Have a nice day");
   }
 }
